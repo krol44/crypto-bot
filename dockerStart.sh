054163ec402bbeeb362f8ff4ea015fe0380a3ab5
krol44 <dancebass@gmail.com>
2021-08-01 20:00:28 +0300

    new bot

--- dockerStart.sh
+++ dockerStart.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 docker build . -t crypto-bot-container
 docker rm -f crypto-bot
-docker run -d --restart=always --log-opt max-size=50m —-name=crypto-bot crypto-bot-container
+docker run -d --restart=always --log-opt max-size=50m --name=crypto-bot crypto-bot-container
+docker prune -f
